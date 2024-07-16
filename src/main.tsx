f64303dadce811a957490b74cf4b7d347c557e92
Alan Laredo <AlanLaredo@hotmail.com>
2024-07-16 15:30:54 -0600

    feat: add routes

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,51 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'users',
+        element: <div>users</div>,
+        children: [
+          {
+            path: 'create',
+            element: <div>create user</div>,
+          },
+          {
+            path: ':user',
+            element: <div>user $user</div>,
+          },
+          {
+            path: ':user/edit',
+            element: <div>edit</div>,
+          },
+          {
+            path: ':user/delete',
+            element: <div>delete</div>,
+          },
+        ],
+      },
+      {
+        path: 'news',
+        element: <div>news</div>,
+        children: [
+          {
+            path: ':new',
+            element: <div>View New</div>,
+          },
+        ],
+      },
+    ],
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
